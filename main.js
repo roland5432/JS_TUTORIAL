5268eb85cb61efb2e94067df43e2e6c4ea470841
Roland Clark <roland5432@hotmail.com>
2020-11-06 13:59:33 -0500

    Start of tutorial

---
+++ main.js
@@ -0,0 +1,19 @@
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meeting with boss',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist',
+        isCompleted: false
+    }
+];
+
+console.log(todos[1].text); //returns Meeting with boss
